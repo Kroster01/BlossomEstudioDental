7c127360d6a489163eb1082d0faab5bb9d0000fd
Jose Hernandez <jhernandez@kiosolutions.com>
2022-10-01 15:39:22 -0300

    primera parte de interpolación de componente edit

--- BlossomEstudioDental_web/src/app/pages/pagos/edit/edit.component.ts
+++ BlossomEstudioDental_web/src/app/pages/pagos/edit/edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit',
@@ -10,13 +11,15 @@ export class EditComponent implements OnInit {
   @Input() item: any;
   @Output() ButtonClick = new EventEmitter<any>();
   element: any;
+  pagosForm!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     console.log('Init app-edit : item');
     console.log(JSON.stringify(this.item));
     this.element = this.item.state.value;
+    this.initForm();
   }
 
   volver(): void {
@@ -33,4 +36,15 @@ export class EditComponent implements OnInit {
     alert('se guarda el item: ' + this.element);
   }
 
+  private initForm(): void {
+    this.pagosForm = this.fb.group({
+      NomProfecional: ['', [Validators.required]],
+      descripcion: ['', [Validators.required]],
+      fecha: ['', [Validators.required]],
+      monto: ['', [Validators.required]],
+      estado: ['', [Validators.required]],
+      observacion: ['', [Validators.required]],
+      pagoHonorarios: ['', [Validators.required]]
+    });
+  }
 }
